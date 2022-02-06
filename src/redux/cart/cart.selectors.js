5d19e044b5debcf168357cd92e8b618145a3611d
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2022-02-06 23:32:28 +1100

    Checkout Page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,7 +16,16 @@ export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) =>
     cartItems.reduce(
-      (accumulatedQuantity, carItem) => accumulatedQuantity + carItem.quantity,
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
       0
     )
+);
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accumulatedQuantity, cartItem) =>
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
+    0
+  )
 );
