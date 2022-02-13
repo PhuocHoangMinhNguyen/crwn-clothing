b6e90c7afc3f6f963942e05da8557bfd3e794f7c
PhuocHoangMinhNguyen <phuochoangminh3699@gmail.com>
2022-02-13 22:27:15 +1100

    Collection Overview Component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -8,15 +8,13 @@ import MenuItem from "../menu-item/menu-item.component";
 
 import "./directory.style.scss";
 
-const Directory = ({ sections }) => {
-  return (
-    <div className="directory-menu">
-      {sections.map(({ id, ...otherSectionProps }) => (
-        <MenuItem key={id} {...otherSectionProps} />
-      ))}
-    </div>
-  );
-};
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
   sections: selectDirectorySections,
